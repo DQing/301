9db586b1e042e12e7c81db20ff702573f3d6b98e
douqing <qqdou@thoughtworks.com>
2018-01-23 11:56:03 +0800

    part

--- src/app/App.js
+++ src/app/App.js
@@ -1,21 +1,156 @@
 import React, {Component} from 'react';
-import {Button} from 'antd'
-import './App.less';
+import {Layout, Input, Form, Checkbox, Card, Icon, Modal} from 'antd'
+import './App.less'
+import addButton from '../static/addButton.png';
+
+const {Header, Footer, Content} = Layout;
+
+const FormItem = Form.Item;
+const {TextArea} = Input
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            easy: '',
+            general: '',
+            difficult: '',
+            visible: false
+        }
+    }
+
+    onHandleChange() {
+        document.getElementsByClassName("inputs-group")[0].removeAttribute("hidden")
+    }
+
+    onNumberChange = (e) => {
+
+        const reg = /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/;
+        const value = document.getElementsByName(e)[0].value;
+        if ((!isNaN(value) && reg.test(value) && e === 'easy') || value === '') {
+            this.setState({easy: value})
+        } else if ((!isNaN(value) && reg.test(value) && e === 'general') || value === '') {
+            this.setState({general: value})
+        } else if ((!isNaN(value) && reg.test(value) && e === 'difficult') || value === '') {
+            this.setState({difficult: value})
+        }
+    };
+
+    showTopicOption() {
+        this.setState({
+            visible: true
+        })
+
+    }
+
+    handleOk() {
+
+    }
+
+    handleCancel() {
+
+    }
+
     render() {
+        const {getFieldDecorator} = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: {span: 24},
+                sm: {span: 4, offset: 4},
+            },
+            wrapperCol: {
+                xs: {span: 24},
+                sm: {span: 10},
+            },
+        };
         return (
             <div className="App">
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
-                <Button type="danger">Danger</Button>
-                <p className="test">
-                    sdofja
-                </p>
+                <Layout>
+                    <Header className="app-header">
+                        <span>新增试卷</span>
+                    </Header>
+                    <Content className="app-content">
+                        <div className="top-content">
+                            <Form name="myForm">
+                                <FormItem
+                                    {...formItemLayout}
+                                    label="试卷名称"
+                                >
+                                    {getFieldDecorator('name', {
+                                        rules: [{
+                                            max: true, message: '试卷名称长度不能超过32个字符'
+                                        }],
+                                    })(
+                                        <Input placeholder="请输入试卷名称"/>
+                                    )}
+                                </FormItem>
+                                <FormItem
+                                    {...formItemLayout}
+                                    label="试卷描述"
+                                >
+                                    {getFieldDecorator('examDescription', {
+                                        rules: [{
+                                            max: true, message: '试卷描述长度不能超过255个字符'
+                                        }],
+                                    })(
+                                        <TextArea placeholder="请输入试卷描述"/>
+                                    )}
+                                </FormItem>
+                                <FormItem
+                                    {...formItemLayout}
+                                    label="逻辑题"
+                                >
+                                    <Checkbox onChange={this.onHandleChange}/>
+                                    <div className="inputs-group" hidden>
+                                        <span className="type">简单</span>
+                                        <Input size="large" name="easy"
+                                               onChange={this.onNumberChange.bind(this, 'easy')}
+                                               value={this.state.easy}/>
+                                        <span className="type">一般</span>
+                                        <Input size="large" name="general"
+                                               onChange={this.onNumberChange.bind(this, 'general')}
+                                               value={this.state.general}/>
+                                        <span className="type">困难</span>
+                                        <Input size="large" name="difficult"
+                                               onChange={this.onNumberChange.bind(this, 'difficult')}
+                                               value={this.state.difficult}/>
+                                    </div>
+
+                                </FormItem>
+
+                            </Form>
+                        </div>
+                        <div className="center-line">
+                            <hr/>
+                        </div>
+                        <div className="bottom-content">
+                            <Card title="编程题"
+                                  extra={<span>
+                                      <Icon type="form"/>
+                                      <Icon type="delete"/>
+                                  </span>}>
+                                <div className="topic-list">
+                                    <div className="add-button">
+                                        <img src={addButton} alt="add-button"
+                                             onClick={this.showTopicOption.bind(this)}/>
+                                    </div>
+                                    <div className="modal">
+                                        <Modal
+                                            title="Basic Modal"
+                                            visible={this.state.visible}
+                                            onOk={this.handleOk}
+                                            onCancel={this.handleCancel}/>
+                                    </div>
+                                </div>
+                            </Card>
+                        </div>
+                    </Content>
+                    <Footer>Footer</Footer>
+                </Layout>
             </div>
         );
     }
 }
 
-export default App;
+const WrappedRegistrationForm = Form.create()(App);
+export default WrappedRegistrationForm;
