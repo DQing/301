cbeb46e2ddec2fe0ff34955aecfa07417dd1dc1d
douqing <qqdou@thoughtworks.com>
2018-01-23 10:05:14 +0800

    use less and antd init

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-import './App.css';
+import {Button} from 'antd'
+import './App.less';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+                <p className="test">
+                    sdofja
+                </p>
             </div>
         );
     }
